ba14bcd47f1533350e8512e5f6e32f5ae585abce
Trisna Chandra Laksana <57003845+Chandralaksana22@users.noreply.github.com>
2024-06-25 23:31:01 +0800

    Update Add Placetogo

---
+++ pages/placetogo.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="bg-cover bg-[url('../assets/image/bg-desktop.png')]">
+       <div class="p-5">
+        <h1 class="boldfont text-white text-lg text-center">Places To Go</h1>
+        <NuxtLink to="/detail-place-to-go" rel="noopener noreferrer">
+            <img class="mt-5" src="../assets/image/mbalizoo.png" alt="">
+        </NuxtLink>
+        <NuxtLink to="/detail-place-to-go" rel="noopener noreferrer">
+            <img class="mt-5" src="../assets/image/mbalibird.png" alt="">
+        </NuxtLink>
+        <NuxtLink to="/detail-place-to-go" rel="noopener noreferrer">
+            <img class="mt-5" src="../assets/image/mbalifarm.png" alt="">
+        </NuxtLink>
+        <NuxtLink to="/detail-place-to-go" rel="noopener noreferrer">
+            <img class="mt-5" src="../assets/image/mbalisafari.png" alt="">
+        </NuxtLink>
+       </div>
+    </section>
+</template>
